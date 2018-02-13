b2fe582bfc8e717bc158dc79b6b15a868d76faad
John Samuel <eng.john86@gmail.com>
2018-02-13 14:08:17 +0200

    Starting Forums Project

---
+++ Forums/main.py
@@ -0,0 +1,8 @@
+from Forums.models import Member,Post
+
+member1 = Member("John",32)
+member2 = Member("Ahmed",25)
+
+post1 = Post("Topic1", "This is the first topic !!!")
+post2 = Post("Topic2", "This is the second topic !!!")
+post3 = Post("Topic3", "This is the third topic !!!")
---
+++ Forums/models.py
@@ -0,0 +1,11 @@
+class Member():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        print("Member Class is initialized successfully !!")
+
+class Post():
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+        print("Post Class is initialized successfully !!")
