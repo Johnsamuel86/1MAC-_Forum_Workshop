bc2fbae8cadbe486dbf27203a9ae5706c64d6cb6
John Samuel <eng.john86@gmail.com>
2018-02-15 00:00:18 +0200

    Made changes on Stores

--- Forums/main.py
+++ Forums/main.py
@@ -1,31 +1,35 @@
 from Forums.models import Member,Post
 from Forums.stores import MemberStore,PostStore
 
-member1 = Member("John", 32)
-MemberStore.add(member1.name)
-member2 = Member("Ahmed", 25)
-MemberStore.add(member2.name)
+memberstore = MemberStore()
+poststore = PostStore()
 
-print(MemberStore.get_all())
+member1 = Member("John", 32)
+memberstore.add(member1.name)
+member2 = Member("Ahmed", 25)
+memberstore.add(member2.name)
+
+print(memberstore.get_all())
 
 post1 = Post("Topic1", "This is the first topic !!!")
-PostStore.create(member1.name, post1.title, post1.content, post1.uid)
+poststore.create(member1.name, post1.title, post1.content, post1.uid)
 post2 = Post("Topic2", "This is the second topic !!!")
-PostStore.create(member1.name, post2.title, post2.content, post2.uid)
+poststore.create(member1.name, post2.title, post2.content, post2.uid)
 post3 = Post("Topic3", "This is the third topic !!!")
-PostStore.create(member2.name, post3.title, post3.content, post3.uid)
+poststore.create(member2.name, post3.title, post3.content, post3.uid)
 
 
-print(PostStore.read("Topic2"))  # can be done with Id but it is easier with title
-print(PostStore.read("Topic3"))  # can be done with Id but it is easier with title
-print(PostStore.read("Topic"))   # should give error msg
-print(PostStore.read("Topic1"))  # can be done with Id but it is easier with title
+print(poststore.read("Topic2"))  # can be done with Id but it is easier with title
+print(poststore.read("Topic3"))  # can be done with Id but it is easier with title
+print(poststore.read("Topic"))   # should give error msg
+print(poststore.read("Topic1"))  # can be done with Id but it is easier with title
 
+print(poststore.get_all())
 
-print(PostStore.update("Topic2", "This is the new topic after modified !!"))
-print(PostStore.update("Topic", "This topic isn't exist yet !!"))
+print(poststore.update("Topic2", "This is the new topic after modified !!"))
+print(poststore.update("Topic", "This topic isn't exist yet !!"))  # should give error msg
 
-print(PostStore.delete("Topic"))  # should give error msg
-print(PostStore.delete("Topic3"))
+print(poststore.delete("Topic"))  # should give error msg
+print(poststore.delete("Topic3"))
 
-print(PostStore.posts)
+print(poststore.get_all())
--- Forums/stores.py
+++ Forums/stores.py
@@ -1,21 +1,22 @@
 class MemberStore:
     members = []
 
-    @staticmethod
-    def get_all():
+
+    def get_all(self):
         return MemberStore.members
 
-    @staticmethod
-    def add(member):
+    def add(self, member):
         MemberStore.members.append(member)
 
 
 class PostStore:
     posts = []
 
-    @staticmethod
-    def create(member_name, post_title, post_content, uid):
+    def create(self, member_name, post_title, post_content, uid):
         PostStore.posts.append([member_name, post_title, post_content, uid])
+
+    def get_all(self):
+        return PostStore.posts
 
     @staticmethod
     def find_post(title):
@@ -24,22 +25,19 @@ class PostStore:
                 return post
 
 
-    @staticmethod
-    def read(title):
+    def read(self, title):
         if PostStore.find_post(title) is not None:
             return PostStore.find_post(title)
         return "No post found that match your input to read"
 
-    @staticmethod
-    def update(title, new_content):
+    def update(self, title, new_content):
         post = PostStore.find_post(title)
         if post is not None:
             post[2] = new_content
             return post
         return "No post found that match your input to update"
 
-    @staticmethod
-    def delete(title):
+    def delete(self, title):
         post = PostStore.find_post(title)
         if post is not None:
             return PostStore.posts.pop(PostStore.posts.index(post))
